68b6eccb99e74aeb21991a642afb67c36fc4a0bd
ansh-garewal <ansh.garewal@flamapp.com>
2023-07-14 01:04:40 +0530

    Minor change

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -17,13 +17,7 @@ export class AppController {
     return "test";
   }
 
-  @Get('docs')
-  @Redirect('https://docs.nestjs.com', 302)
-  getDocs(@Query('version') version) {
-    if (version && version === '5') {
-      return { url: 'https://docs.nestjs.com/v5/' };
-    }
-  }
+
 
   @Get(':id')
   findOne(@Param() params: any): string {
